317e9af524543f9426340668f590ecafd596805b
barbara42 <barbara.raven42@gmail.com>
2014-03-29 09:08:00 -0400

    finish button added

--- questions/urls.py
+++ questions/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import patterns, url
 from questions import views
 
 urlpatterns = patterns('',
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
     url(r'^(?P<topic_id>\d+)/$', views.detail, name='detail'),
+    url(r'^finish/$', views.finish, name='finish'),
 )
--- questions/views.py
+++ questions/views.py
@@ -10,11 +10,25 @@ def index(request):
 def detail(request, topic_id):
 	#gets the topic associated with the id number
 	topic = Topic.objects.get(pk=topic_id)
+	#gets the next topic id and info 
+	next_topic_id = int(topic_id) + 1
+	next_topic_text = Topic.objects.get(pk=topic_id).topic
+ 	#if there are no more topics, go to finish
+ 	if next_topic_text == topic.topic:
+		next_topic_id = 'finish'
+		next_topic_text = 'finish'
+	print next_topic_id
+	print next_topic_text
 	#gets the questions associated with that topic 
 	question_list = topic.question_set.all()
 	template = loader.get_template('questions/detail.html')
 	context = RequestContext(request, {
 	    'question_list': question_list,
 	    'topic': topic.topic,
+	    'next_topic_id': next_topic_id,
+	    'next_topic_text': next_topic_text,
 	})
 	return HttpResponse(template.render(context))
+
+def finish(request):
+	return HttpResponse("Thank you for playing!")
