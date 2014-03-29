df82dc08470e790d8f255fd4ab649cf544a9f5ef
barbara42 <barbara.raven42@gmail.com>
2014-03-29 16:17:36 -0400

    Login/logout updated. Users fleshed out

--- questions/admin.py
+++ questions/admin.py
@@ -10,3 +10,22 @@ class TopicAdmin(admin.ModelAdmin):
 
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Test)
+
+#adding admin features for the user extension 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from questions.models import UserExtension
+
+class ExtensionInline(admin.StackedInline):
+    model = UserExtension
+    can_delete = False
+    verbose_name_plural = 'User'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (ExtensionInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
